a418ee77a3fd2a1d13e7bcb15a490edc60baeeba
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2025-07-13 22:22:31 +0100

    update the styles of browser warning

--- src/components/browser-warning.tsx
+++ src/components/browser-warning.tsx
@@ -17,23 +17,28 @@ export function BrowserWarning() {
         width: '100vw',
         height: '100vh',
         background: 'rgba(0,0,0,0.85)',
-        color: '#fff',
+        color: '#faf2fe',
         zIndex: 9999,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: '2rem',
         textAlign: 'center',
         padding: '2rem',
+        backdropFilter: 'blur(2px)',
       }}
     >
-      <h2>Unsupported Browser</h2>
-      <p>
-        This CV works best in a Chromium-based browser (such as Chrome, Edge, or
-        Brave).
-      </p>
-      <p>Please switch to a Chromium browser for the best experience.</p>
+      <h2 style={{ fontSize: '2rem' }}>Unsupported Browser</h2>
+      <div style={{ maxWidth: '600px' }}>
+        <p style={{ fontSize: '1.2rem', textWrap: 'balance' }}>
+          This CV works best in a Chromium-based browser (such as Chrome, Edge,
+          or Brave).
+        </p>
+        <br />
+        <p style={{ fontSize: '1.2rem' }}>
+          Please switch to a Chromium browser for the best experience.
+        </p>
+      </div>
     </div>
   ) : null
 }
