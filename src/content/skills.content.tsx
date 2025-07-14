ed6bf0da3241e3ee166e20017185f0aca014b22b
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2025-07-14 16:45:13 +0100

    more exhaustive skill list

--- src/content/skills.content.tsx
+++ src/content/skills.content.tsx
@@ -1,7 +1,7 @@
-import { Skills, Exhaustive } from './types'
+import { Skills, Exhaustive, Practices, Tools, Languages } from './types'
 
 export type SkillDetail = {
-  text: Skills
+  text: Languages | Tools | Practices
   expert?: true
   exhaustive?: Exhaustive.exhaustive
 }
@@ -12,8 +12,10 @@ export const skills: MySkills = {
   languages: [
     { text: Skills.bash },
     { text: Skills.css, expert: true, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.coffee, exhaustive: Exhaustive.exhaustive },
     { text: Skills.golang, exhaustive: Exhaustive.exhaustive },
     { text: Skills.html, expert: true, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.handlebars, exhaustive: Exhaustive.exhaustive },
     { text: Skills.java, exhaustive: Exhaustive.exhaustive },
     {
       text: Skills.javaScript,
@@ -31,6 +33,7 @@ export const skills: MySkills = {
     { text: Skills.apache, exhaustive: Exhaustive.exhaustive },
     { text: Skills.aws },
     { text: Skills.backbone, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.circleCi },
     { text: Skills.cypress, expert: true },
     { text: Skills.django, exhaustive: Exhaustive.exhaustive },
     { text: Skills.docker },
@@ -41,13 +44,18 @@ export const skills: MySkills = {
     { text: Skills.gcp, exhaustive: Exhaustive.exhaustive },
     { text: Skills.git, expert: true },
     { text: Skills.gitHub, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.gitLab, exhaustive: Exhaustive.exhaustive },
     { text: Skills.graphQl, exhaustive: Exhaustive.exhaustive },
     { text: Skills.grpc, exhaustive: Exhaustive.exhaustive },
     { text: Skills.grunt, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.gulp, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.jenkins, exhaustive: Exhaustive.exhaustive },
     { text: Skills.jest, exhaustive: Exhaustive.exhaustive },
     { text: Skills.jira, exhaustive: Exhaustive.exhaustive },
     { text: Skills.jQuery, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.karma, exhaustive: Exhaustive.exhaustive },
     { text: Skills.kubernetes, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.launchDarkly, exhaustive: Exhaustive.exhaustive },
     { text: Skills.langchain },
     { text: Skills.less, exhaustive: Exhaustive.exhaustive },
     { text: Skills.marionette, exhaustive: Exhaustive.exhaustive },
@@ -55,6 +63,7 @@ export const skills: MySkills = {
     { text: Skills.mongoDb, exhaustive: Exhaustive.exhaustive },
     { text: Skills.mySql, exhaustive: Exhaustive.exhaustive },
     { text: Skills.nest, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.newrelix, exhaustive: Exhaustive.exhaustive },
     { text: Skills.next },
     { text: Skills.numpy, exhaustive: Exhaustive.exhaustive },
     { text: Skills.nx, exhaustive: Exhaustive.exhaustive },
@@ -62,13 +71,17 @@ export const skills: MySkills = {
     { text: Skills.playwright, exhaustive: Exhaustive.exhaustive },
     { text: Skills.postgreSql },
     { text: Skills.protobuf, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.protractor, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.ramda, exhaustive: Exhaustive.exhaustive },
     { text: Skills.react, expert: true },
     { text: Skills.redis, exhaustive: Exhaustive.exhaustive },
     { text: Skills.redux, expert: true, exhaustive: Exhaustive.exhaustive },
-    { text: Skills.sagas, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.reduxSagas, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.sentry, exhaustive: Exhaustive.exhaustive },
     { text: Skills.swagger, exhaustive: Exhaustive.exhaustive },
     { text: Skills.terraform, exhaustive: Exhaustive.exhaustive },
     { text: Skills.tailwind, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.twilio, exhaustive: Exhaustive.exhaustive },
     { text: Skills.vite, exhaustive: Exhaustive.exhaustive },
     { text: Skills.webpack, exhaustive: Exhaustive.exhaustive },
     { text: Skills.wordpress, exhaustive: Exhaustive.exhaustive },
@@ -76,17 +89,26 @@ export const skills: MySkills = {
   ],
   practices: [
     { text: Skills.advancedTesting, expert: true },
+    { text: Skills.abTesting },
     { text: Skills.aiEngineering },
     { text: Skills.architecture, expert: true },
+    { text: Skills.apiDesign, expert: true },
     { text: Skills.bem, expert: true, exhaustive: Exhaustive.exhaustive },
     { text: Skills.browserCompat, expert: true },
     { text: Skills.cd, expert: true },
-    { text: Skills.continuousIntegration, exhaustive: Exhaustive.exhaustive },
+    {
+      text: Skills.continuousIntegration,
+      expert: true,
+      exhaustive: Exhaustive.exhaustive,
+    },
     { text: Skills.crossFunctionalLead, expert: true },
     { text: Skills.deepLearning, exhaustive: Exhaustive.exhaustive },
     { text: Skills.designSystems, expert: true },
     { text: Skills.documentation, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.eventSourcing, exhaustive: Exhaustive.exhaustive },
     { text: Skills.extremeProg, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.functional, expert: true },
+    { text: Skills.gitflow, exhaustive: Exhaustive.exhaustive },
     { text: Skills.internationalisation },
     { text: Skills.kanban, exhaustive: Exhaustive.exhaustive },
     { text: Skills.machineLearning, exhaustive: Exhaustive.exhaustive },
@@ -94,13 +116,22 @@ export const skills: MySkills = {
     { text: Skills.microFrontends, exhaustive: Exhaustive.exhaustive },
     { text: Skills.microservices },
     { text: Skills.monorepos, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.openIdConnect, exhaustive: Exhaustive.exhaustive },
     { text: Skills.observability },
     { text: Skills.owasp },
     { text: Skills.rag },
+    { text: Skills.redBlue },
     { text: Skills.rest, expert: true },
     { text: Skills.scrum, expert: true },
+    { text: Skills.schemaDesign, expert: true },
     { text: Skills.tdd, expert: true },
     { text: Skills.technicalStrategy, expert: true },
+    { text: Skills.technicalWriting, exhaustive: Exhaustive.exhaustive },
+    { text: Skills.ux, exhaustive: Exhaustive.exhaustive },
     { text: Skills.wcag, expert: true },
   ],
 }
+
+/*
+twilio
+*/
--- src/content/types.ts
+++ src/content/types.ts
@@ -29,11 +29,12 @@ export type Period = {
   to?: string
 }
 
-export enum Skills {
-  // languages
+export enum Languages {
   bash = 'Bash',
   css = 'CSS',
+  coffee = 'coffeescript',
   golang = 'Golang',
+  handlebars = 'Handlebars',
   html = 'HTML',
   java = 'Java',
   javaScript = 'JavaScript',
@@ -43,12 +44,15 @@ export enum Skills {
   python = 'Python',
   scss = 'SCSS',
   typeScript = 'TypeScript',
-  // tools
+}
+
+export enum Tools {
   akamai = `Akamai`,
   angular = 'Angular',
   apache = `Apache`,
   aws = 'AWS',
   backbone = 'BackboneJS',
+  circleCi = 'CircleCI',
   cypress = 'Cypress',
   django = 'Django',
   docker = 'Docker',
@@ -59,19 +63,25 @@ export enum Skills {
   gcp = 'GCP',
   git = 'Git',
   gitHub = 'GitHub',
+  gitLab = 'GitLab',
   graphQl = 'GraphQL',
   grpc = 'GRPC',
   grunt = 'Grunt',
+  gulp = 'Gulp',
+  jenkins = 'Jenkins',
   jest = 'Jest',
   jira = 'Jira',
   jQuery = 'jQuery',
+  karma = 'Karma',
   kubernetes = 'Kubernetes',
+  launchDarkly = 'LaunchDarkly',
   langchain = 'Langchain',
   less = 'LESS',
   marionette = 'MarionetteJS',
   mocha = 'Mocha',
   mongoDb = 'MongoDB',
   nest = 'Nest.js',
+  newrelix = 'NewRelic',
   next = 'Next.js',
   nightwatch = 'Nightwatch',
   numpy = 'Numpy',
@@ -80,21 +90,29 @@ export enum Skills {
   playwright = 'Playwright',
   postgreSql = 'PostgreSQL',
   protobuf = 'Protobuf',
+  protractor = 'Protractor',
+  ramda = 'Ramda',
   react = 'React',
   redis = 'Redis',
   redux = 'Redux',
+  reduxSagas = `Redux sagas`,
   rx = 'RxJS',
-  sagas = `Redux sagas`,
+  sentry = `Sentry`,
   swagger = 'Swagger',
   tailwind = 'Tailwind',
   terraform = 'Terraform',
+  twilio = 'Twilio',
   vite = 'Vite',
   webpack = 'Webpack',
   wordpress = 'Wordpress',
   zend = 'Zend PHP',
-  // practices
+}
+
+export enum Practices {
+  abTesting = 'A/B testing',
   advancedTesting = 'Advanced testing',
   aiEngineering = 'AI Engineering',
+  apiDesign = 'API design',
   architecture = 'Architecture',
   bem = 'BEM',
   browserCompat = 'Browser compatibility',
@@ -104,7 +122,10 @@ export enum Skills {
   deepLearning = 'Deep Learning',
   designSystems = 'Design Systems',
   documentation = `Documentation`,
+  eventSourcing = `Event sourcing`,
   extremeProg = `Extreme programming`,
+  functional = `Functional programming`,
+  gitflow = `Gitflow`,
   internationalisation = `Internationalisation`,
   kanban = `Kanban`,
   machineLearning = 'Machine Learning',
@@ -113,11 +134,24 @@ export enum Skills {
   microservices = `Microservices`,
   monorepos = `Monorepos`,
   observability = `Observability`,
+  openIdConnect = `OpenID Connect`,
   owasp = `OWASP`,
   rag = `RAG`,
+  redBlue = `Red blue deploys`,
   rest = 'REST',
   scrum = 'Scrum',
+  schemaDesign = 'Schema design',
   tdd = `TDD`,
   technicalStrategy = 'Technical strategy',
+  technicalWriting = 'Technical writing',
+  ux = 'User Experience',
   wcag = 'WCAG 2.1 compliance',
 }
+
+export const Skills = {
+  ...Languages,
+  ...Tools,
+  ...Practices,
+}
+
+export type Skills = (typeof Skills)[keyof typeof Skills]
